91a67c2cb229dd5b7e9888b48dcd86d358b61d67
Blake Kaufman <blakekaufman9@gmail.com>
2025-06-24 09:31:12 -0400

    created loading spinner

---
+++ src/components/spinner/spinner.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+export default function ActivityIndicator({ width, height }) {
+  return (
+    <div
+      style={{
+        width,
+        height,
+      }}
+      id="activityIndicator"
+    />
+  );
+}
