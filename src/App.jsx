95a12665653fac8435061d62de99ad3acb922ad3
Blake Kaufman <blakekaufman9@gmail.com>
2025-06-24 09:31:39 -0400

    added screens and fix styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import WelcomScreen from "./pages/welcome/welcome";
+import RestoreScreen from "./pages/restore/restore";
+import WalletScreen from "./pages/wallet/wallet";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentState, setCurrentState] = useState("home");
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  const handleStateChange = (newState) => {
+    if (newState === currentState || isTransitioning) return;
+
+    setIsTransitioning(true);
+
+    setTimeout(() => {
+      setCurrentState(newState);
+      setIsTransitioning(false);
+    }, 500); //css animation duration
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="recoveryContainer">
+      <WelcomScreen
+        currentState={currentState}
+        handleStateChange={handleStateChange}
+        isTransitioning={isTransitioning}
+      />
+      <RestoreScreen
+        currentState={currentState}
+        handleStateChange={handleStateChange}
+        isTransitioning={isTransitioning}
+      />
+      <WalletScreen
+        currentState={currentState}
+        handleStateChange={handleStateChange}
+        isTransitioning={isTransitioning}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./fonts.css";
 import App from "./App.jsx";
+import { SparkWalletProvider } from "./contexts/sparkContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <SparkWalletProvider>
+      <App />
+    </SparkWalletProvider>
   </StrictMode>
 );
